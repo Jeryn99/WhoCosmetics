90f0553cca53ae16cecc784e956ec4d4da7afb4a
BellatorHD <fredrik.l.skogli@hotmail.com>
2020-08-02 15:31:28 +0200

    Initial Commit

---
+++ src/main/java/com/whocraft/whocosmetics/WhoCosmetics.java
@@ -0,0 +1,41 @@
+package com.whocraft.whocosmetics;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+
+@Mod(WhoCosmetics.MODID)
+public class WhoCosmetics
+{
+
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public static final String MODID = "whocosmetics";
+
+    public WhoCosmetics() {
+
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+        MinecraftForge.EVENT_BUS.register(this);
+
+    }
+
+    public void doClientStuff(final FMLClientSetupEvent event) {
+;
+    }
+
+}
