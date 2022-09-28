d3a55161dd361cba977a2815711b4e23ca879334
Craig <sandedshoes@gmail.com>
2022-09-28 22:20:16 +0100

    Assets

---
+++ common/src/main/java/me/craig/software/cosmetics/registry/CustomRegistry.java
@@ -0,0 +1,30 @@
+package me.craig.software.cosmetics.registry;
+
+import dev.architectury.injectables.annotations.ExpectPlatform;
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Collection;
+import java.util.Set;
+
+public abstract class CustomRegistry<T> {
+
+    @ExpectPlatform
+    public static <T> CustomRegistry<T> create(Class<T> clazz, ResourceLocation id) {
+        throw new AssertionError();
+    }
+
+    public abstract ResourceKey<? extends Registry<T>> getRegistryKey();
+
+    public abstract T get(ResourceLocation key);
+
+    public abstract ResourceLocation getKey(T object);
+
+    public abstract Set<ResourceLocation> getKeys();
+
+    public abstract boolean containsKey(ResourceLocation key);
+
+    public abstract Collection<T> getValues();
+
+}
