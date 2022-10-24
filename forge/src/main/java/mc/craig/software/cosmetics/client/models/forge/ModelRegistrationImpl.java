efbfae06f4739af775652f91e2a40895808fa9cc
Craig <sandedshoes@gmail.com>
2022-10-24 10:15:29 +0100

    No more crash, but code needs cleaned

--- forge/src/main/java/mc/craig/software/cosmetics/client/models/forge/ModelRegistrationImpl.java
+++ forge/src/main/java/mc/craig/software/cosmetics/client/models/forge/ModelRegistrationImpl.java
@@ -1,5 +1,6 @@
 package mc.craig.software.cosmetics.client.models.forge;
 
+import mc.craig.software.cosmetics.client.models.ModelRegistration;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.builders.LayerDefinition;
 import net.minecraftforge.client.event.EntityRenderersEvent;
@@ -13,11 +14,13 @@ public class ModelRegistrationImpl {
     public static final Map<ModelLayerLocation, Supplier<LayerDefinition>> DEFINITIONS = new ConcurrentHashMap<>();
 
     public static ModelLayerLocation register(ModelLayerLocation location, com.google.common.base.Supplier<LayerDefinition> definition) {
+        System.out.println(location);
         DEFINITIONS.put(location, definition);
         return location;
     }
 
     public static void registerToGame(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        ModelRegistration.init();
         DEFINITIONS.forEach(event::registerLayerDefinition);
     }
 
